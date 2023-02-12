b0b414f15bf6e2611685e9c7790c78915da739ab
Zac <zbenattar@gmail.com>
2023-02-12 20:19:33 +0000

    Improved basic pages
    Details page now shows all of a user's info.
    Attempted to get a favicon working but browser still can't seem to find the file

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 urlpatterns = [
+    # Not working yet
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
+    
 ]
