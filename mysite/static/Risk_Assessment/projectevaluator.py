615a042227355c21d869f9f642a1bcd669533321
JacobAdams101 <jacob.adams2003@hotmail.co.uk>
2023-02-13 15:01:19 +0000

    Updated
    ...

--- mysite/static/Risk_Assessment/projectevaluator.py
+++ mysite/static/Risk_Assessment/projectevaluator.py
@@ -2,7 +2,11 @@ from sklearn import datasets
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 
+#Used to store a loaded linear model and update/save it
+class ProjectEvaluator:
 
 
-def get_riskiness():
-    return 0
+
+    #This will take in a projects riskiness and return a number between 0 and 1 which can be used as a percentage
+    def get_riskiness():
+        return 0
--- mysite/static/Risk_Assessment/projectsuggester.py
+++ mysite/static/Risk_Assessment/projectsuggester.py
@@ -1,3 +1,5 @@
 #Alex can you make a function in here that makes suggestions for the project
 #Look at the design document for a potential implementation
 #Thanks, Jacob
+def get_suggestions(): #Feel free to add parameters
+    return None #change this to return an object
