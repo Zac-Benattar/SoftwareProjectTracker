cd6ff72eee1a12074cb833330352218c35f25b76
alex1162 <92046330+alex1162@users.noreply.github.com>
2023-02-16 17:53:46 +0000

    Update projectsuggester.py

--- mysite/static/Risk_Assessment/projectsuggester.py
+++ mysite/static/Risk_Assessment/projectsuggester.py
@@ -1,5 +1,23 @@
 #Alex can you make a function in here that makes suggestions for the project
 #Look at the design document for a potential implementation
 #Thanks, Jacob
-def get_suggestions(): #Feel free to add parameters
-    return None #change this to return an object
+
+import datetime
+from users.models import Project, Task
+
+# retrieve all instances
+all_Projects = Project.objects.all()
+all_Tasks = Task.objects.all()
+
+def get_suggestions(project): 
+
+    # evaluating project's pararameters to make suggestions
+
+    # iterate over the instances
+    for task in all_Tasks:  
+        if task.project == project:
+            if task.completionStatus != 'F':
+                if datetime.date.today() > task.Deadline:
+                    print(f'You are running out of time to complete the task {task.name}. Try allocating more people to the task.')
+
+    return None
