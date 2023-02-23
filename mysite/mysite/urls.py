34210764619006c6f2f898efc89b6d8bffe7457e
Zac <zbenattar@gmail.com>
2023-02-23 17:59:06 +0000

    Revert "Revert "Merge branch 'DJANGO-+-REACT-+-API'""
    This reverts commit 759c22e3116ee5d8df3d3d0febc54ededae1963e.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+#from django.views.generic import TemplateView
 
 urlpatterns = [
     # Not working yet
@@ -24,5 +25,7 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('projects/', include('projects.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    # path('', TemplateView.as_view(template_name = 'index.html')),
     
 ]
