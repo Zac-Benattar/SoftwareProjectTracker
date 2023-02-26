f7945562aeeae17bd1509962fa0379cfab898309
Zac <zbenattar@gmail.com>
2023-02-26 16:55:10 +0000

    Moved homepage.jsx to pages
    Deleted old homepage.jsx

--- mysite/frontend/web-app/src/pages/Homepage.jsx
+++ mysite/frontend/web-app/src/pages/Homepage.jsx
@@ -1,33 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { Header } from "../components/Header";
-import { Navbar } from '../components/Navbar';
-import { ProjectListItem } from '../components/ProjectListItem';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import "./App.css";
+import { Progress_bar } from "../components/ProgressBar";
+import { Dropdown } from "../components/Dropdown";
+import { Navbar } from "../components/Navbar";
+import { ProjectListItem } from "../components/ProjectListItem";
 
-export const Homepage = (props) => {
+export const Homepage = () => {
+  const options = [
+    { value: "green", label: "Green" },
+    { value: "blue", label: "Blue" },
+  ];
 
-    let [projects, setProjects] = useState([])
+  let [projects, setProjects] = useState([]);
 
-    useEffect(() => {
-        getProjects()
-    }, [])
+  useEffect(() => {
+    getProjects();
+  }, []);
 
-    let getProjects = async () => {
-        let response = await fetch('/api/projects/')
-        let data = await response.json()
-        console.log('Data:', data)
-        setProjects(data)
-    }
+  let getProjects = async () => {
+    let response = await fetch("/api/projects/");
+    let data = await response.json();
+    console.log("Data:", data);
+    setProjects(data);
+  };
 
-    return (
+  return (
+    <div>
+      <Navbar />
+
+      <div className="home-page">
+        <div className="dropdown-menu">
+          <Dropdown placeHolder="Select ..." options={options} />
+        </div>
+
         <div>
-            <Navbar />
-            <div className="projects-list">
-                {projects.map((project, index)=> (
-                    <ProjectListItem key={index} project={project}/>
-                ))}
-            </div>
-        </div >
-
-    )
-}
+          <div className="projects-list">
+            {projects.map((project, index) => (
+              <ProjectListItem key={index} project={project} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
