58391b760d1022fac2009dc12c634e28cff75528
loisperryman02 <loisperryman02@gmail.com>
2023-02-26 22:49:38 +0000

    Updated home page so information from database is displayed

--- mysite/frontend/web-app/src/components/ProjectListItem.js
+++ mysite/frontend/web-app/src/components/ProjectListItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ProgressBar } from "./ProgressBar";
+import { Homepage } from "../pages/Homepage";
 
 export const ProjectListItem = ({ project }) => {
   return (
@@ -7,14 +8,14 @@ export const ProjectListItem = ({ project }) => {
       <div className="title">
         <h1>{project.name}</h1>
       </div>
-      <div>
-        <ProgressBar progress="100" />
+      <div className="progress-bar">
+        <ProgressBar progress="30" />
       </div>
       <div className="info_container">
         <div className="info">
           <p>Current Budget: £{project.current_budget}</p>
           <p>Current Deadline: {project.current_deadline}</p>
-          <p>{project.methodology}</p>
+          <p>Methodology: {project.methodology}</p>
         </div>
         <div className="info">
           <p>Here there will be some info</p>
--- mysite/frontend/web-app/src/pages/Homepage.jsx
+++ mysite/frontend/web-app/src/pages/Homepage.jsx
@@ -4,11 +4,56 @@ import "./Homepage.css";
 import { Dropdown } from "../components/Dropdown";
 import { Navbar } from "../components/Navbar";
 import { ProjectListItem } from "../components/ProjectListItem";
+import {ProgressBar} from '../components/ProgressBar';
 
+// export const Homepage = () => {
+//   const options = [
+//     {value: "name", label:"Project Name"},
+//     {value: "deadline_date", label: "Deadline"},
+//     {value: "start_date", label: "Start Date"},
+//     {value: "progress", label: "Progress completed"}
+// ];
+
+//   let [projects, setProjects] = useState([]);
+
+//   useEffect(() => {
+//     getProjects();
+//   }, []);
+
+//   let getProjects = async () => {
+//     let response = await fetch("/api/projects/");
+//     let data = await response.json();
+//     console.log("Data:", data);
+//     setProjects(data);
+//   };
+
+//   return (
+//     <div>
+//       <Navbar />
+
+//       <div className="home-page">
+//         <div className="dropdown-menu">
+//           <Dropdown placeHolder="Select ..." options={options} />
+//         </div>
+
+//         <div>
+//           <div className="projects-list">
+//             {projects.map((project, index) => (
+//               <ProjectListItem key={index} project={project} />
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 export const Homepage = () => {
+
   const options = [
-    { value: "green", label: "Green" },
-    { value: "blue", label: "Blue" },
+      {value: "name", label:"Project Name"},
+      {value: "deadline_date", label: "Deadline"},
+      {value: "start_date", label: "Start Date"},
+      {value: "progress", label: "Progress completed"}
   ];
 
   let [projects, setProjects] = useState([]);
@@ -25,22 +70,106 @@ export const Homepage = () => {
   };
 
   return (
-    <div>
-      <Navbar />
-
+      <>
       <div className="home-page">
-        <div className="dropdown-menu">
-          <Dropdown placeHolder="Select ..." options={options} />
-        </div>
 
-        <div>
-          <div className="projects-list">
-            {projects.map((project, index) => (
-              <ProjectListItem key={index} project={project} />
-            ))}
-          </div>
-        </div>
+      <div className="user-profile"> 
+
+      This will contain some user information
+
+
       </div>
-    </div>
+
+     
+      <div className="top-of-page"> 
+
+          <h2 className="dropdown-title"> 
+              ORDER BY:
+          </h2>
+          <div className="dropdown-menu">
+
+              <Dropdown placeHolder="Select ..." options={options}/>
+
+          </div>
+
+      </div>
+
+
+
+        
+      <div className="projects-list">
+        {projects.map((project, index) => (
+          <ProjectListItem key={index} project={project} />
+        ))}
+      </div>
+
+      <div className="project_container">
+          
+          <div className="title">
+              <h1>Demo Project</h1>
+          </div>
+          <div className="progress-bar">
+              <ProgressBar progress='30'/>
+          </div>
+          
+          <div className="info_container">
+              <div className="info">
+                  <p>Here there will be some info</p>
+                  <p>Some more info </p>
+                  <p>Some other info</p>
+              </div>
+              <div className="info">
+                  <p>Here there will be some info</p>
+                  <p>Some more info </p>
+                  <p>Some other info</p>
+              </div>
+          </div>
+      </div> 
+      <div className="project_container">
+          <div className="title">
+              <h1>Demo Project</h1>
+          </div>
+          <div className="progress-bar">
+              <ProgressBar progress='100'/>
+          </div>
+          <div className="info_container">
+              <div className="info">
+                  <p>Here there will be some info</p>
+                  <p>Some more info </p>
+                  <p>Some other info</p>
+              </div>
+              <div className="info">
+                  <p>Here there will be some info</p>
+                  <p>Some more info </p>
+                  <p>Some other info</p>
+              </div>
+          </div>
+      </div>  
+      <div className="project_container">
+          <div className="title">
+              <h1>Demo Project</h1>
+          </div>
+          <div className="progress-bar">
+              <ProgressBar progress='70'/>
+          </div>
+          <div className="info_container">
+              <div className="info">
+                  <p>Here there will be some info</p>
+                  <p>Some more info </p>
+                  <p>Some other info</p>
+              </div>
+              <div className="info">
+                  <p>Here there will be some info</p>
+                  <p>Some more info </p>
+                  <p>Some other info</p>
+              </div>
+          </div>
+      </div> 
+
+      </div>
+
+     
+      </>     
+   
   );
-};
+}
