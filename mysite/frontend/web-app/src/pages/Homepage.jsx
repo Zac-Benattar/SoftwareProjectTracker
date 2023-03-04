c5cac6201f3cbd0911ae0f4809103f234a8963ae
loisperryman02 <loisperryman02@gmail.com>
2023-03-04 15:32:40 +0000

    Added option to assign members and their roles to a project.

--- mysite/frontend/web-app/src/pages/Homepage.jsx
+++ mysite/frontend/web-app/src/pages/Homepage.jsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./Homepage.css";
 import { Dropdown } from "../components/Dropdown";
 import { ProjectListItem } from "../components/ProjectListItem";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
-import ReactDOM from "react-dom";
-
-import Navbar from "../components/Navbar";
 import HomeNavbar from "../components/HomeNavbar";
+import MemberInput from "../components/MemberInput";
 
 
 
@@ -20,22 +18,42 @@ export const Homepage = () => {
       {value: "progress", label: "Progress completed"}
   ];
 
-  const member_options = [
-    {value: "user1", label:"User1"},
-    {value: "user2", label:"User2"}
-];
-
   const [projectName, setProjectName] = useState("");
   const [projectDescription, setProjectDescription] = useState("");
   const [projectBudget, setProjectBudget] = useState("");
   const [projectStart, setProjectStart] = useState("");
   const [projectDeadline, setProjectDeadline] = useState("");
   const [projectMembers, setProjectMembers] = useState("");
+  const [roleName, setRoleName] = useState("");
+  const [roleDescription, setRoleDescription] = useState("");
+
+  // list to store members in a project
+  const [membersList, setMembersList] = useState([{member: ""}]); 
 
 
+  
+
+  console.log(membersList);
+  const handleServiceAdd = () => {
+    setMembersList([...membersList, {member:""}]);
+  }
+
+  const handleServiceRemove = (index) => {
+    const list = [...membersList];
+    list.splice(index, 1);
+    setMembersList(list);
+  }
+
+  const handleServiveChange = (e,  index) => {
+    const {name, value} = e.target; 
+    const list = [...membersList];
+    list[index][name]=value;
+    setMembersList(list);
+  }
 
 
   const [date,setDate] = useState(new Date());
+
   let [projects, setProjects] = useState([]);
 
   useEffect(() => {
@@ -49,9 +67,43 @@ export const Homepage = () => {
     setProjects(data);
   };
 
+
+  let [allRoles, setRoles] = useState([]);
+
+  useEffect(() => {
+    getRoles();
+  }, []);
+
+  let getRoles = async () => {
+    let response = await fetch("/api/roles/");
+    let data = await response.json();
+    console.log("Data:", data);
+    setRoles(data);
+  };
+
+  const roleNames = 
+  allRoles.map(role =>( { key:role.id}, {label:role.name} ));
+
+  console.log(roleNames);
+
+
   const addProject = () => {
     var modal = document.getElementById("add-project-modal");
     var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    }
+
+  }
+  const addRole = () => {
+    var modal = document.getElementById("add-role-modal");
+    var span = document.getElementsByClassName("role-close")[0];
     modal.style.display = "block";
     span.onclick = function() {
       modal.style.display = "none";
@@ -68,23 +120,24 @@ export const Homepage = () => {
   return (
       <>
       <HomeNavbar/> 
-
-
       <div id="add-project-modal" className="modal">
         <div className="modal-content"> 
-        <span className="close">&times;</span>
-                <h1>Create New Project</h1>
-                <span className = "project_name">
-                    <label forHtml = "project_name" className = "inputLabels">Project Name: </label>
-                    <input 
-                    type = "text" 
-                    required id = "project_name" 
-                    name = "project_name" 
-                    placeholder = "Project Name" 
-                    className = "projectInputs" 
-                    onChange={event=> setProjectName(event.target.value)}>
-                    </input>
-                </span>
+              
+              <span className="close">&times;</span>
+
+              <h1>Create New Project</h1>
+
+              <span className = "project_name">
+                  <label forHtml = "project_name" className = "inputLabels">Project Name: </label>
+                  <input 
+                  type = "text" 
+                  required id = "project_name" 
+                  name = "project_name" 
+                  placeholder = "Project Name" 
+                  className = "projectInputs" 
+                  onChange={event=> setProjectName(event.target.value)}>
+                  </input>
+              </span>
 
                 <span className = "project_description">
                     <label forHtml = "project_description" className = "inputLabels">Project Description: </label>
@@ -150,33 +203,73 @@ export const Homepage = () => {
                       className = "projectInputs" 
                       onChange = {event => setProjectStart(event.target.value)}>
                     </input>
-                </span>
+                </span>           
 
-                <span className = "project_members">
-                    
-                    <label 
-                          forHtml = "project_members"
-                          className = "inputLabels">
-                            Project Members: 
-                    </label>
-                    
+                <label htmlFor="member"> Member(s) </label>
+                {membersList.map((singleMember,index) => (
+                   <>                  
+                   <div key = {index} className="members">
+                  <input name="member" type="text" id="member" placeholder="Member Username" required
+                  value = {singleMember.member}
+                  onChange={(e)=>handleServiveChange(e, index )}/>
+                  {membersList.length - 1 === index   && 
+                  (
+                  <button onClick={handleServiceAdd}> <span> Add a member </span></button>
+                  )}
 
-                    <div className = "projectInputs" >
-                    <Dropdown
-                      required id = "project_members" 
-                      name = "project_members" 
-                      placeholder = "Add Members" 
-                      onChange = {event => setProjectMembers(event.target.value)}
-                      options = {member_options}/>
-                    </div>
+                  <Dropdown classname="add-proj-drop" placeHolder="Select Role" options={roleNames}/>
+
+                  <button onClick={addRole}> Role not here? Add a new one. </button>
+                  </div>
                   
-                </span>
-
+                  <div className="second-div">
+                    <button onClick={()=>handleServiceRemove(index)}> <span> Remove Button </span></button>
+                  </div>
+                  </>
+                  
+                ))}
+ 
                 <span>
                   <button className="create-project-btn">Create Project</button>
                 </span>
                 
         </div>
+      </div>
+
+
+      <div className="new-role-modal" id="add-role-modal">
+
+      <div className="role-modal-content"> 
+              
+              <span className="role-close">&times;</span>
+              <span className = "role-name">
+                  <label forHtml = "role-name" className = "role-input-labels">Role Name: </label>
+                  <input 
+                  type = "text" 
+                  required id = "role_name" 
+                  name = "role_name" 
+                  placeholder = "Role Name" 
+                  className = "projectInputs" 
+                  onChange={event=> setRoleName(event.target.value)}>
+                  </input>
+              </span>
+
+                <span className = "role-description">
+                    <label forHtml = "role-description" className = "role-input-labels">Role Description: </label>
+                    <input 
+                    type = "text" 
+                    required id = "role_description" 
+                    name = "role_description" 
+                    placeholder = "Role Description" 
+                    className = "projectInputs" 
+                    onChange = {event => setRoleDescription(event.target.value)}>
+                    </input>
+                </span>
+
+                <button> Add new role! </button>
+
+      </div>
+      
       </div>
       
       <div className="home-page">
