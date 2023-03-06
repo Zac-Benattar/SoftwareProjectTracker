5472b407b919a08e20c97722cfa6b9a47c329f3b
Zac <zbenattar@gmail.com>
2023-03-06 16:59:46 +0000

    Updated navbar

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -9,7 +9,6 @@ from rest_framework.decorators import permission_classes, action
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
