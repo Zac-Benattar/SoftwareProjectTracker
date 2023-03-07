8aadcd098e37bc13a230265a14bb23449a6e2124
loisperryman02 <loisperryman02@gmail.com>
2023-03-07 18:53:36 +0000

    Updating people page

--- mysite/frontend/web-app/src/components/ListPeople.jsx
+++ mysite/frontend/web-app/src/components/ListPeople.jsx
@@ -7,37 +7,58 @@ import {Link} from "react-router-dom";
 export const ListPeople = ({ member }) => {
 
 
-    // let [users, setUsers] = useState([]);
+    let [user, setUsers] = useState([]);
+    let [role, setRoles] = useState([]);
+    let [skill, setSkills] = useState([]);
 
-    // useEffect(() => {
-    //     getUsers();
-    // }, []);
+    useEffect(() => {
+        getUsers();
+        getRoles();
+        getSkills();
+    }, []);
 
-    // let getUsers = async () => {
-    //     let response = await fetch('/api/users/'+member.id+'/');
-    //     let data = await response.json();
-    //     console.log("Data:", data);
-    //     setUsers(data);
-    // };
+    let getUsers = async () => {
+        let response = await fetch('/api/users/'+member.id+'/');
+        let data = await response.json();
+        console.log("Data:", data);
+        setUsers(data);
+    };   
 
-    console.log("member");
+    let getRoles = async () => {
+        let response = await fetch('/api/roles/'+member.role+'/');
+        let data = await response.json();
+        console.log("Data:", data);
+        setRoles(data);
+    };
 
+    let getSkills = async () => {
+        let response = await fetch('/api/roles/'+user.skillset+'/');
+        let data = await response.json();
+        console.log("Data:", data);
+        setSkills(data);
+    };
+
+
+
+
+    console.log(user);
+    console.log("roles",role);
+
+    console.log("skill",skill);
 
   return (
-  
-
 
         <div className="people-container">
             <div className="people-info">
-               
-                <h3> {member.role} </h3> 
-                        
+               <h2> Name: {user.first_name} </h2>
+                <h3> Role: {role.name} </h3> 
+                    
                      <ul className="all-skills">
                         <li className="skill">
-                            {member.skillset}
+                            {skill.name}
                         </li>
                     </ul>
-                     
+                    <p> Work hours: {member.work_hours}</p>
                     <p>{member.phone}</p>
                     <p>{member.email}</p>                    
             </div>
