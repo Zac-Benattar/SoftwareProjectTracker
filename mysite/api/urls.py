bba0a1d30393c55ddb7bd761236d074b6a6c6fb6
Zac <zbenattar@gmail.com>
2023-03-08 12:59:48 +0000

    Updated projectsuggester.py
    Project suggester now generates suggestion objects and pushes them to the database, returning all suggestions when get_suggestions is called

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -14,53 +14,56 @@ router.register(r'skills', SkillViewSet)
 
 
 # project routers , api call : /api/projects/pk/model_name/pk
-member_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
-member_router.register( r'members', MemberViewSet, basename='project-member')
+members_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+members_router.register( r'members', MemberViewSet, basename='project-members')
 
-task_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
-task_router.register( r'tasks', TaskViewSet, basename='project-task')
+tasks_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+tasks_router.register( r'tasks', TaskViewSet, basename='project-tasks')
 
-not_started_task_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
-not_started_task_router.register( r'notstartedtasks', NotStartedTaskViewSet, basename='project-task-not-started')
+not_started_tasks_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+not_started_tasks_router.register( r'notstartedtasks', NotStartedTaskViewSet, basename='project-tasks-not-started')
 
-started_task_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
-started_task_router.register( r'startedtasks', StartedTaskViewSet, basename='project-task-started')
+started_tasks_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+started_tasks_router.register( r'startedtasks', StartedTaskViewSet, basename='project-tasks-started')
 
-finished_task_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
-finished_task_router.register( r'finishedtasks', FinishedTaskViewSet, basename='project-task-finished')
+finished_tasks_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+finished_tasks_router.register( r'finishedtasks', FinishedTaskViewSet, basename='project-tasks-finished')
 
-no_status_task_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
-no_status_task_router.register( r'nostatustasks', NoStatusTaskViewSet, basename='project-task-no-status')
+no_status_tasks_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+no_status_tasks_router.register( r'nostatustasks', NoStatusTaskViewSet, basename='project-tasks-no-status')
 
-risk_evaluation_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
-risk_evaluation_router.register( r'riskevaluation', RiskEvaluationViewSet, basename='project-riskEvaluation')
+risk_evaluations_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+risk_evaluations_router.register( r'riskevaluations', RiskEvaluationViewSet, basename='project-riskEvaluations')
 
 generate_risk_evaluation_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
 generate_risk_evaluation_router.register( r'generateriskevaluation', RiskEvaluationGeneratorViewSet, basename='project-generateRiskEvaluation')
 
-meeting_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
-meeting_router.register( r'meeting', MeetingViewSet, basename='project-meeting')
+meetings_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+meetings_router.register( r'meetings', MeetingViewSet, basename='project-meetings')
 
 feedback_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
 feedback_router.register( r'feedback', FeedbackViewSet, basename='project-feedback')
 
-recommendation_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
-recommendation_router.register( r'recommendation', RecommendationViewSet, basename='project-recommendation')
+recommendations_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+recommendations_router.register( r'recommendations', RecommendationViewSet, basename='project-recommendations')
+
+generate_recommendation_router = routers.NestedDefaultRouter( router, r'projects', lookup = 'project')
+generate_recommendation_router.register( r'generaterecommendations', RecommendationViewSet, basename='project-generateRecommendations')
 
 # member routers , api call : /api/projects/pk/members/pk/model_name/pk
-schedule_router = routers.NestedDefaultRouter( member_router, r'members', lookup = 'member')
-schedule_router.register( r'schedule', ScheduleViewSet, basename='member-schedule')
+schedules_router = routers.NestedDefaultRouter( member_router, r'members', lookup = 'member')
+schedules_router.register( r'schedules', ScheduleViewSet, basename='member-schedules')
 
 timeWorked_router = routers.NestedDefaultRouter( member_router, r'members', lookup = 'member')
 timeWorked_router.register( r'timeworked', TimeWorkedViewSet, basename='member-timeWorked')
 
 # role routers , api call : /api/roles/pk/model_name/pk
-role_requirement_router = routers.NestedDefaultRouter(router, r'roles', lookup = 'role')
-role_requirement_router.register(r'rolerequirement', RoleRequirementViewSet, basename = 'role-roleRequirement')
+role_requirements_router = routers.NestedDefaultRouter(router, r'roles', lookup = 'role')
+role_requirements_router.register(r'rolerequirements', RoleRequirementViewSet, basename = 'role-roleRequirements')
 
 # user routers , api call : /api/users/pk/skill/pk
 userSkills_router = routers.NestedDefaultRouter( router, r'users', lookup = 'user')
-userSkills_router.register( r'skills', UserSkillViewSet, basename='user-userSkill')
+userSkills_router.register( r'skills', UserSkillViewSet, basename='user-userSkills')
 
 # user routers , api call : /api/users/myaccount
 userAccount_router = routers.NestedDefaultRouter( router, r'users', lookup = 'user')
@@ -70,20 +73,21 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
     path('', include(userSkills_router.urls)),
-    path('', include(role_requirement_router.urls)),
+    path('', include(role_requirements_router.urls)),
     path('', include(timeWorked_router.urls)),
-    path('', include(member_router.urls)),
-    path('', include(schedule_router.urls)),
-    path('', include(task_router.urls)),
-    path('', include(not_started_task_router.urls)),
-    path('', include(started_task_router.urls)),
-    path('', include(finished_task_router.urls)),
-    path('', include(no_status_task_router.urls)), 
-    path('', include(risk_evaluation_router.urls)),
+    path('', include(members_router.urls)),
+    path('', include(schedules_router.urls)),
+    path('', include(tasks_router.urls)),
+    path('', include(not_started_tasks_router.urls)),
+    path('', include(started_tasks_router.urls)),
+    path('', include(finished_tasks_router.urls)),
+    path('', include(no_status_tasks_router.urls)), 
+    path('', include(risk_evaluations_router.urls)),
     path('', include(generate_risk_evaluation_router.urls)),
-    path('', include(meeting_router.urls)),
+    path('', include(meetings_router.urls)),
     path('', include(feedback_router.urls)),
-    path('', include(recommendation_router.urls)),
+    path('', include(recommendations_router.urls)),
+    path('', include(generate_recommendation_router.urls)),
     path('', include(userAccount_router.urls)),
     path('token/', MyTokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
