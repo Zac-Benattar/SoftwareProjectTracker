20aeaaaba3c075517dcf709ec388eda5cc4c3422
alex1162 <92046330+alex1162@users.noreply.github.com>
2023-03-09 17:51:16 +0000

    Update tests.py

--- mysite/projects/tests.py
+++ mysite/projects/tests.py
@@ -9,17 +9,17 @@ class tester(TestCase):
 
         # CREATING A PROJECT
         project1 = Project.objects.create(name = "SE_project", initial_budget = 40000,
-                                          current_budget = 3000, amount_spent = 0, initial_deadline = "2023-05-21T23:59:59+01:00",
-                                          current_deadline = "2023-05-21T23:59:59+01:00")
+                                          current_budget = 50, amount_spent = 0, initial_deadline = "2023-05-21T23:59:59+00:00",
+                                          current_deadline = "2023-05-21T23:59:59+00:00")
 
         # CREATING A TASK
         task1 = Task.objects.create(project = project1, name = "task 1",
-                                    duration = 30, creation_date =  get_today_datetime(),
+                                    duration = 30, creation_date =  get_today_datetime(), completion = 0,
                                     completion_status = "S")
 
         # CREATING FEEDBACK
         feedback1 = Feedback.objects.create(project = project1, confidence = 5, \
-                                            emotion = 5, date = get_today_datetime())
+                                            emotion = 2, date = get_today_datetime())
 
         # CREATING A ROLE
         role1 = Role.objects.create(name = "Project Manager")
@@ -46,15 +46,11 @@ class tester(TestCase):
 
         # CREATING A USER
         user1 = CustomUser.objects.create(phone = '+15555555555')
-        user1.skillset.add(skill1, skill2, skill3, skill4)
+        user1.skillset.add(skill1, skill2, skill3, skill4, skill5)
 
         # CREATING A MEMBER
         member1 = Member.objects.create(role = role1, project = project1, user = user1, work_hours = 40, 
                                     join_date = '2023-01-01', salary = 31285)
-
-        # CREATING FILEPATH OF A CODE
-        file_path = r"C:\Users\alexp\Documents\GitHub\SoftwareProjectTracker\mysite\test\test.py"
-
 
     # TESTING
 
@@ -74,24 +70,30 @@ class tester(TestCase):
 
     # AVERAGE HAPPINESS FUNCTION
     ## function will return the average happiness
-    def test_average_happiness(self, project):
-        test = ProjectSuggester(project)
-        self.assertEqual(test.average_happiness(), 5, 'The fuction is wrong.')
+    def test_low_average_happiness(self):
+        example_project = Project.objects.get(name="SE_project")
+        test = ProjectSuggester()
+        self.assertEqual(test.low_average_happiness(example_project), True, 'The fuction is wrong.')
 
     # COUNT COMMENTS FUNCTION
     ## function will return the number of comments
-    def test_count_comments(self, file_path):
-        test = ProjectSuggester(file_path)
-        self.assertEqual(test.count_comments(), 1, 'The fuction is wrong.')
+    def test_count_comments(self):
+        example_project = Project.objects.get(name="SE_project")
+        # CREATING FILEPATH OF A CODE
+        file_path = r"C:\Users\alexp\Documents\GitHub\SoftwareProjectTracker\mysite\test\test.py"
+        test = ProjectSuggester()
+        self.assertEqual(test.count_comments(file_path, example_project), False, 'The fuction is wrong.')
 
     # COMPLETION RATIO FUNCTION
     ## function will return TRUE if the suggestion has to be displayed
-    def test_completion_ratio(self, project):
-        test = ProjectSuggester(project)
-        self.assertEqual(test.changing_roles(), True, 'The fuction is wrong.')
+    def test_low_completion_ratio(self):
+        example_project = Project.objects.get(name="SE_project")
+        test = ProjectSuggester()
+        self.assertEqual(test.low_completion_ratio(example_project), False, 'The fuction is wrong.')
 
     # LOW BUDGET FUNCTION
     ## function will return TRUE if the suggestion has to be displayed
-    def test_low_budget(self, project):
-        test = ProjectSuggester(project)
-        self.assertEqual(test.low_budget(), True, 'The fuction is wrong.')
+    def test_low_budget(self):
+        example_project = Project.objects.get(name="SE_project")
+        test = ProjectSuggester()
+        self.assertEqual(test.low_budget(example_project), True, 'The fuction is wrong.')
